36df658273705390081125fe2fb99f50bc6dfab6
Pamela Nachbar <nachbarpamela@gmail.com>
2025-02-09 18:58:30 -0300

    Update

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+console.log('Vai começar...')
+for(var c = 0; c <= 4; c++){
+    console.log(c)
+}
+console.log('Fim')
+
+
+
+
+
+
+/*
+var i = 0
+
+do{
+    console.log(`Passo ${i}`)
+    i++
+} while(i < 6)
+
+
+var i = 0
+
+while(i < 6){
+    console.log(`Passo ${i}`)
+    i++
+}
+
+for(i = 0; i < 6; i++){
+    console.log('Tudo bem?')
+}*/
